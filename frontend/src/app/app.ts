95946d1791f7d282c8d323adc4bb17ce1a44dad5
Sophia-Araujo <sophiaaraujoanjos@22gmail.com>
2025-10-05 20:26:08 -0300

    add

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,15 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, Output, signal } from '@angular/core';
 import { LivroCapa } from './pages/livro-capa/livro-capa';
 import { ChatBaloon } from './components/chat-baloon/chat-baloon';
 import { PaginaAgricultor } from "./pages/pagina-agricultor/pagina-agricultor";
+import { PaginaVovo } from "./pages/pagina-vovo/pagina-vovo";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [LivroCapa, ChatBaloon, PaginaAgricultor],
+  imports: [LivroCapa, ChatBaloon, PaginaAgricultor, PaginaVovo, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('hack25front');
+  protected readonly title = 'hack25front';
+  pagNumber = 1;
+
+  increasePage() {
+    this.pagNumber++;
+  }
+
+  decreasePage() {
+    this.pagNumber--;
+  }
 }
